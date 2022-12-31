a3eb38a211059a728c85a0f1c39650ca849b5af9
boi-doingthings <boi_doingthings@zohomail.in>
2022-12-31 20:46:37 +0530

    added structural changes

--- flask/apps.py
+++ flask/apps.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from pymongo import MongoClient
 app = Flask(__name__)
 
@@ -10,9 +10,9 @@ db = MongoClient(
 
 @app.route('/')
 def main_page():
-    return "This is home."
+    return render_template("profile.html")
 
-@app.route('/login',methods=["GET","POST"])
+@app.route('/login/',methods=["GET","POST"])
 def login_page():
     if request.method =="POST":
         return "Your creds are: {} and {}".format(
@@ -21,6 +21,13 @@ def login_page():
             )
     else:
         return render_template("login.html")
+fields=["Name","instagram","linkedin","facebook"]
+@app.route('/manage/',methods=['GET','POST'])
+def create_profile():
+    if request.method == "GET":
+            return render_template('create_profile.html',len=len(fields),fields=fields)
+    else:
+        return list(request.values.keys())
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000,host='0.0.0.0')
